e9ea48706bf90fab2daea347782db9f35a03bd09
Godfrey Best <godfrey.c.best@gmail.com>
2022-07-16 23:34:09 -0400

    Update linting, formatting, circleci

--- src/server/router/auth.ts
+++ src/server/router/auth.ts
@@ -1,22 +1,23 @@
-import { TRPCError } from "@trpc/server";
-import { createRouter } from "./context";
+import { TRPCError } from '@trpc/server';
+import { createRouter } from './context';
 
 export const authRouter = createRouter()
-  .query("getSession", {
+  .query('getSession', {
     resolve({ ctx }) {
       return ctx.session;
-    }
+    },
   })
   .middleware(async ({ ctx, next }) => {
     // Any queries or mutations after this middleware will
     // raise an error unless there is a current session
     if (!ctx.session) {
-      throw new TRPCError({ code: "UNAUTHORIZED" });
+      throw new TRPCError({ code: 'UNAUTHORIZED' });
     }
     return next();
   })
-  .query("getSecretMessage", {
+  .query('getSecretMessage', {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     async resolve({ ctx }) {
-      return "You are logged in and can see this secret message!";
-    }
+      return 'You are logged in and can see this secret message!';
+    },
   });
